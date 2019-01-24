9e23366fddd4f4bd6ef942a3c0dd2bd1b549a23f
Str3et <paanika@list.ru>
2019-01-24 15:28:59 +0300

    Домашнее задание №2.

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,14 +2,38 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 # Create your views here.
+
+
 def index(request: HttpRequest):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'page_title': 'Home',
+        }
+    return render(request, 'mainapp/index.html', context=context)
+
 
 def contact(request: HttpRequest):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'page_title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', context=context)
+
 
 def interior(request: HttpRequest):
-    return render(request, 'mainapp/interior.html')
+    context = {
+        'page_title': 'Лучшее предложение',
+    }
+    return render(request, 'mainapp/interior.html', context=context)
+
 
 def products(request: HttpRequest):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'page_title': 'Каталог товаров',
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def test(request: HttpRequest):
+    context = {
+        'page_title': 'Тест',
+    }
+    return render(request, 'mainapp/child.html', context=context)
